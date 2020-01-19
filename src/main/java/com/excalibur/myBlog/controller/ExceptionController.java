b53504fb749679e43b7be19ca4ae36a074f36a65
A.Syromyatnikov <a.syromyatnikov@fleetcor.ru>
2020-01-20 00:38:24 +0300

    log in and registration enhancements, security (add missing ctrl)

---
+++ src/main/java/com/excalibur/myBlog/controller/ExceptionController.java
@@ -0,0 +1,17 @@
+package com.excalibur.myBlog.controller;
+
+import com.excalibur.myBlog.utils.Environment;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionController {
+
+    @GetMapping(value = "/error")
+    public String getErrorPage(Model model) {
+        model.addAttribute("errorURI", Environment.getErrorURI());
+        return "error-page";
+    }
+
+}
